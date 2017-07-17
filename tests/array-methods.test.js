0a7b592a10f9bedb0acc6206762af5c1226e21d4
christylaguardia <christinelaguardia@gmail.com>
2017-07-17 09:34:48 -0700

    changed test to deepEqual

--- tests/array-methods.test.js
+++ tests/array-methods.test.js
@@ -6,6 +6,6 @@ describe('array methods', () => {
     it('returns array of squared numbers greater than 20', () => {
         const array = [1, 4, 5, 7];
         const squared = methods.squared(array);
-        assert.equal(squared, [25, 49]);
+        assert.deepEqual(squared, [25, 49]);
     });
 });
