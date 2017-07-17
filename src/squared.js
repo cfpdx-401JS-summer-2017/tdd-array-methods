b4affc68b76fb851c59451fc636b8e585286f018
Meryl Turner <merylturner08@gmail.com>
2017-07-17 09:36:07 -0700

    Removed consoles, reformatted

--- src/squared.js
+++ src/squared.js
@@ -2,7 +2,7 @@ function squared(array) {
     const squared = [];
     for (let i = 0; i < array.length; i++) {
         const squaredVal = array[i] * array[i];
-        console.log(squaredVal);
+
         if (squaredVal > 20) {
             squared.push(squaredVal);
         }
--- tests/squared.test.js
+++ tests/squared.test.js
@@ -3,9 +3,9 @@ const squared = require('../src/squared');
 
 describe('array methods', () => {
     it('squared numbers array', () => {
-        const array = [1,5,8];
-        
+        const array = [1, 5, 8];
+
         const squaredArray = squared(array);
-        assert.deepEqual(squaredArray,[25,64]);
+        assert.deepEqual(squaredArray, [25, 64]);
     });
 });
