e95c1ee53fe1a0473f34c09791f7a46319b2e11a
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-17 09:34:03 -0700

    minor changes

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const source = require('../src/source');
 var assert = require('assert');
 
 describe('map', () => {
-    it('Creates a new array witholing elements that did not pass the test', ()=> {
+    it('Creates a new array changing elements that did not pass the test', ()=> {
         var array = [1, 4, 5, 7]
-        const maped = source.map(array, function(array, i){
+        const maped = source.map(array, function callback(array, i){
            newItem = array[i] * array[i] 
            if (newItem > 20) return newItem
 
